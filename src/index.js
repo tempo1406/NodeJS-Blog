96a83706069cccd475d1c61945664fb84db49c9a
tempo1406 <134191285+tempo1406@users.noreply.github.com>
2025-02-15 16:52:40 +0700

    MVC Routes & Controllers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const express = require("express");
-const { engine } = require("express-handlebars"); // Dùng require thay vì import
+const { engine } = require("express-handlebars"); 
 const morgan = require("morgan");
 const path = require("path");
 
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, "public"))); // Dùng path.join để nối đường dẫn
+const route = require("./routes");
+
+app.use(express.static(path.join(__dirname, "public"))); 
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
 // HTTP logger
 app.use(morgan("combined"));
 
@@ -17,15 +25,8 @@ app.engine("hbs", engine({
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources", "views"));
 
-// Route
-app.get("/", (req, res) => {
-    res.render("home");
-});
-
-app.get("/news", (req, res) => {
-    res.render("news");
-});
-
+route(app);
+ 
 // Start server
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
