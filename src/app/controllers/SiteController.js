0caac7e58af9fc27df9382f111c8c25bb807b353
tempo1406 <134191285+tempo1406@users.noreply.github.com>
2025-02-15 20:18:40 +0700

    MVC Model

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,7 +1,14 @@
+const Course = require('../models/Course');
+
 class SiteController {
     // [GET]
-    index(req, res) {
-        res.render('home');
+    async index(req, res) {
+        try {
+            const courses = await Course.find({});
+            res.json(courses);
+        } catch (err) {
+            res.status(400).json({ error: 'ERROR!!!' });
+        }
     }
 
     // [GET] search
